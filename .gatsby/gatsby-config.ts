245162e6ae372690037c3837048d620c900e8938
DannyGJdeJong <dannygjdejong@gmail.com>
2020-06-19 13:54:01 +0200

    Added S3 Bucket deploy using gatsby

--- .gatsby/gatsby-config.ts
+++ .gatsby/gatsby-config.ts
@@ -2,7 +2,13 @@ import { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
   plugins: [
-    'gatsby-plugin-typescript'
+    'gatsby-plugin-typescript',
+    {
+      resolve: 'gatsby-plugin-s3',
+      options: {
+        bucketName: 'denni.dev',
+      }
+    }
   ]
 };
 
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,6 @@
 const { generateConfig } = require('gatsby-plugin-ts-config');
-const { resolve } = require('path');
 
 module.exports = generateConfig({
-  configDir: resolve(__dirname, 'gatsby'),
+  configDir: '.gatsby',
   babel: true
 });
